4e3ed189dbdc432a0b0732b98999313213f5596a
naoyuki <sawasawa-yu@hotmail.co.jp>
2017-04-07 13:10:32 +0000

    TopPage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  
+  root to: 'toppages#index'
   root to: 'tasks#index'
+  
 
   resources :tasks
 end
