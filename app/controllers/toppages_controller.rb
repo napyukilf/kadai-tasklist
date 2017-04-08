6f4d293fc44af5fe2215869800fcab1d6a22baf2
naoyuki <sawasawa-yu@hotmail.co.jp>
2017-04-08 12:35:02 +0000

    Tasklist subject

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,9 @@
 class ToppagesController < ApplicationController
   def index
+     if logged_in?
+      @user = current_user
+      @tasklist = current_user.tasklists.build
+      @tasklists = current_user.tasklists.order('created_at DESC').page(params[:page])
+    end
   end
 end
--- app/models/tasklist.rb
+++ app/models/tasklist.rb
@@ -1,8 +1,5 @@
-class Task < ApplicationRecord
+class Tasklist < ApplicationRecord
   belongs_to :user
-  
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
-  validates :status, presence: true, length: { maximum: 10 }
-  
 end
